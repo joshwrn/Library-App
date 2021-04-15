064aacafd7d4c7f558e8786e69087ec32051a13d
joshwrn <joshnwarren@gmail.com>
2021-04-14 19:17:57 -0700

    dark mode added

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 /* UI Script */
 
 /* Get Elements */
+const root = document.documentElement;
 const toggle = document.getElementById("toggle");
 const changeToggle = document.getElementById("change-toggle");
+const expandButton = document.getElementById("expand-btn");
+const container = document.getElementById("container");
+
+/* expand button */
+
+let currentSize = "mini";
+expandButton.onclick = function () {
+  if (currentSize == "mini") {
+    container.style.setProperty("width", "1000px");
+    currentSize = "expanded";
+    expandButton.innerHTML = "toggle_on";
+    console.log("expanded");
+  } else {
+    container.style.setProperty("width", "500px");
+    currentSize = "mini";
+    expandButton.innerHTML = "toggle_off";
+    console.log("mini");
+  }
+};
 
 /* Dark Mode Toggle */
 
@@ -11,10 +31,78 @@ toggle.onclick = function () {
   if (currentMode == "light") {
     changeToggle.innerHTML = "toggle_on";
     currentMode = "dark";
+    root.style.setProperty("--container-bg", "rgba(0, 0, 0, 0.644)");
+    root.style.setProperty("--sidebar-bg", "rgba(0, 0, 0, 0.15)");
+    root.style.setProperty("--btn-icon-color", "rgba(255, 255, 255, 0.65)");
+    root.style.setProperty(
+      "--btn-icon-color-hover",
+      "rgb(255, 255, 255) !important;"
+    );
+    root.style.setProperty(
+      "--btn-bg-color-hover",
+      "rgb(0, 0, 0 0.479) !important;"
+    );
+    root.style.setProperty("--newlol", "0px 0px 5px rgba(0, 0, 0, 0.2)");
+    root.style.setProperty(
+      "--sidebar-btn-shadow-hover",
+      "0px 1px 5px rgba(0, 0, 0, 0.205);"
+    );
+    root.style.setProperty("--book-shadow", "rgba(0, 0, 0, 0.479)");
+    root.style.setProperty(
+      "--book-shadow-hover",
+      "0px 3px 10px rgba(0, 0, 0, 0.205);"
+    );
+    root.style.setProperty(
+      "--border-color",
+      "border: 1px solid rgba(0, 0, 0, 0.281)"
+    );
+    root.style.setProperty("--medium-btn-bg-color", "rgba(0, 0, 0, 0.322)");
+    root.style.setProperty("--input-place-color", "rgba(255, 255, 255, 0.5)");
+    root.style.setProperty(
+      "--input-textarea-color",
+      "rgba(255, 255, 255, 0.75)"
+    );
+    root.style.setProperty("--input-focus-color", "rgba(145, 145, 145, 0.38)");
+    root.style.setProperty("--input-box-color", "rgba(145, 145, 145, 0.25)");
+    root.style.setProperty("--page-heading-color", "rgba(255, 255, 255, 0.75)");
     console.log("dark mode enabled");
   } else {
     changeToggle.innerHTML = "toggle_off";
     currentMode = "light";
+    root.style.setProperty("--container-bg", "rgba(255, 255, 255, 0.644)");
+    root.style.setProperty("--sidebar-bg", "rgba(255, 255, 255, 0.15)");
+    root.style.setProperty("--btn-icon-color", "rgba(0, 0, 0, 0.65)");
+    root.style.setProperty(
+      "--btn-icon-color-hover",
+      "rgb(0, 0, 0) !important;"
+    );
+    root.style.setProperty(
+      "--btn-bg-color-hover",
+      "rgb(255, 255, 255 0.479) !important;"
+    );
+    root.style.setProperty("--newlol", " 0 0px 5px rgba(105, 105, 105, 0.2);");
+    root.style.setProperty(
+      "--sidebar-btn-shadow-hover",
+      "0 1px 5px rgba(105, 105, 105, 0.205);"
+    );
+    root.style.setProperty("--book-shadow", "rgba(0, 0, 0, 0.479)");
+    root.style.setProperty(
+      "--book-shadow-hover",
+      "0 3px 10px rgba(31, 31, 31, 0.205);"
+    );
+    root.style.setProperty(
+      "--border-color",
+      "border: 1px solid rgba(0, 0, 0, 0.281)"
+    );
+    root.style.setProperty(
+      "--medium-btn-bg-color",
+      "rgba(255, 255, 255, 0.322)"
+    );
+    root.style.setProperty("--input-place-color", "rgba(0, 0, 0, 0.5)");
+    root.style.setProperty("--input-textarea-color", "rgba(0, 0, 0, 0.75)");
+    root.style.setProperty("--input-focus-color", "rgba(145, 145, 145, 0.38)");
+    root.style.setProperty("--input-box-color", "rgba(145, 145, 145, 0.25)");
+    root.style.setProperty("--page-heading-color", "rgba(0, 0, 0, 0.75)");
     console.log("light mode enabled");
   }
 };
